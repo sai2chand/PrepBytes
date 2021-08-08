f9a5695c61f5b22e6457531f899d8619c663416f
sai2chand <sai2chand@gmail.com>
2021-08-08 19:25:43 +0530

    Create printtwonumbers.java

---
+++ printtwonumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      //write your code here
+      Scanner in = new Scanner(System.in);
+      int n,m;
+      n = in.nextInt();
+      m = in.nextInt();
+      System.out.println(n+" "+m);
+      
+    }
+  }
